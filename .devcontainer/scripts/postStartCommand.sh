0cfb4a7c8c1a33ecd1bbc83d0e0fd2ed0411249d
Kevin NZUGUEM <nzuguemkevine@gmail.com>
2025-03-02 10:42:06 +0000

    🏷️ Add pluto

--- .devcontainer/scripts/postStartCommand.sh
+++ .devcontainer/scripts/postStartCommand.sh
@@ -4,15 +4,22 @@ set -e
 
 # Configure Bash
 cat <<EOF >> /home/vscode/.bashrc
+
+## Completion
 source <(fzf --bash)
 source <(argocd completion bash)
-## Using ArgoCD CLI on Core Mode (https://docs.openshift.com/gitops/1.12/gitops_cli_argocd/argocd-gitops-cli-reference.html#core-mode)
+source <(pluto completion bash)
+
+## Aliases
+### Using ArgoCD CLI on Core Mode (https://docs.openshift.com/gitops/1.12/gitops_cli_argocd/argocd-gitops-cli-reference.html#core-mode)
 alias argocd="KUBECONFIG=$HOME/.kube/config-argocd argocd --core"
 alias kubectl=kubecolor
 alias k=kubectl
 complete -o default -F __start_kubectl k
+
 export PATH="${KREW_ROOT:-/home/vscode/.krew}/bin:$PATH"
 EOF
+
 sudo sh -c 'task --completion bash > /etc/bash_completion.d/task'
 
 # Deploy and configure cluster
