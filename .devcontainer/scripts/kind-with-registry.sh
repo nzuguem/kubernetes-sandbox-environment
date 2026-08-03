b1dcb499ee3033c5b3b48db0b3f8815df567eb4c
nzuguem <nzuguemkevine@gmail.com>
2026-08-03 06:34:05 +0200

    Configure externally-access registry

--- .devcontainer/scripts/kind-with-registry.sh
+++ .devcontainer/scripts/kind-with-registry.sh
@@ -20,7 +20,8 @@ reg_name='kind-registry'
 reg_port='5001'
 if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
   docker run \
-    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
+    -d --restart=always -p "${reg_port}:5000" --network bridge --name "${reg_name}" \
+    -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
     registry:3
 fi
 
